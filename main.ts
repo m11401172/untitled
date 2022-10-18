f48fbfed93152f27ebe146a7d86efe00f0044b4e
m11401172 <115518096+m11401172@users.noreply.github.com>
2022-10-18 11:18:07 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,19 @@
 basic.forever(function () {
     if (input.lightLevel() > 100) {
-    	
+        pins.servoWritePin(AnalogPin.P2, 180)
     } else {
-    	
+        pins.servoWritePin(AnalogPin.P2, 0)
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        pins.servoWritePin(AnalogPin.P1, 1)
+        basic.pause(200)
+        pins.servoWritePin(AnalogPin.P1, 0)
+        basic.pause(200)
+        pins.servoWritePin(AnalogPin.P1, 1)
+        basic.pause(200)
+        pins.servoWritePin(AnalogPin.P1, 0)
+        basic.pause(200)
     }
 })
